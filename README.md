7ae464f5ed672c74c28d7c85aa5e512cb20ce252
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2024-10-19 20:00:29 -0400

    Modify everything
    Modified everything because it was annoying me.
    Need to finish adding functionality so that when you pick up the phone, you get the details from an array, display the details in a toast, and then pass those details to the generateTicket function.
    🤠

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 A web application that simulates what someone may see working at a help desk. I do not have help desk experience currently so I will be making assumptions. I am making this project to get some experience in both web development and to see what it might be like to work as a help desk technician.
 
 ⚡
+
+## Pseudo code
+
+### Function to answer the phone
+
+LET calls = an array of calls asking for help\
+
+getPhoneCall(call) {\
+    FOR EACH call
+        RETURN call details in a pop up
+    ENDFOR
+}
--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('yo')
+console.log('yo');
+const toastTrigger = document.getElementById('liveToastBtn');
+const toastLiveExample = document.getElementById('liveToast');
+const callTypes = [
+    { type: 'low', details: 'A caller asks question 1' },
+    { type: 'medium', details: 'A caller asks question 2' },
+    { type: 'high', details: 'A caller asks question 3' },
+    { type: 'critical', details: 'A caller asks question 4' }
+];
+
+function getCallFromArray(array) {
+    let type = array.type;
+    let details = array.details;
+    return type, details;
+}
+
+let newCall = getCallFromArray(callTypes);
+
+function showNewCall(call) {
+    const toast = new bootstrap.Toast(toastLiveExample);
+
+    toast.show();
+
+}
+
+
+
+function makeNewTicket(call) {
+
+}
