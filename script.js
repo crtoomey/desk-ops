5ad1508051a4a0d4ecebabb9b16305e5d4134f46
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2024-10-20 11:48:04 -0400

    Modify HTML, CSS, and js
    Modified index.html slightly.
    Modified style.css to make the app look a bit nicer.
    Modified script.js to add classes to the tickets so that they are styled properly.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 console.log('yo');
-let offcanvasCallDetails = document.querySelector('.call-placeholder');
+let callPlaceholder = document.querySelector('.call-placeholder');
 let ticketPlaceholder = document.querySelector('.ticket-placeholder');
 let ticketContainer = document.querySelector('.ticket-container');
 let offcanvasCallContainer = document.querySelector('.call-placeholder-container');
 let callButton = document.querySelector('.call-button');
+let callAlert = document.querySelector('.call-alert');
 let clarifyButton = document.querySelector('.clarify-button');
 let genTicketButton = document.querySelector('.generate-ticket-button');
+let currentCall = null;
+
 const callTypes = [
     { type: 'low', question: 'A caller asks question 1', details: 'More in depth info 1' },
     { type: 'medium', question: 'A caller asks question 2', details: 'More in depth info 2' },
@@ -15,21 +18,69 @@ const callTypes = [
 
 function getCallFromArray(array) {
     let randIndex = Math.floor(Math.random() * array.length);
-    return array[randIndex];
+    let call = array[randIndex];
+    array.splice(randIndex, 1);
+    return call;
 }
 
 
 callButton.addEventListener('click', function () {
-    let newCall = getCallFromArray(callTypes);
-    offcanvasCallDetails.remove();
-    offcanvasCallContainer.innerHTML += `<p>User asks: ${newCall.question}</p>`;
-    clarifyButton.addEventListener('click', function () {
-        offcanvasCallContainer.innerHTML += `<p>User asks: ${newCall.details}</p>`;
-        clarifyButton.remove();
-        genTicketButton.classList.remove("d-none");
-    });
-    genTicketButton.addEventListener('click', function () {
-        ticketPlaceholder.remove();
-        ticketContainer.innerHTML += `<strong>${newCall.type}</strong> <span class="caller-question">${newCall.question}</span> <br /> <span class="caller-details">${newCall.details}</span>`;
-    })
+    currentCall = getCallFromArray(callTypes);
+    callPlaceholder.innerHTML = "";
+    if (currentCall != null) {
+        offcanvasCallContainer.innerHTML += `<p>User asks: ${currentCall.question}</p>`;
+        callPlaceholder.remove();
+        clarifyButton.onclick = function () {
+            offcanvasCallContainer.innerHTML += `<p>User says: ${currentCall.details}</p>`;
+            clarifyButton.classList.add("d-none");
+            genTicketButton.classList.remove("d-none");
+        };
+        genTicketButton.onclick = function () {
+            ticketPlaceholder.remove();
+            let typeClass = '';
+            let typeBorder = '';
+            if (currentCall.type === 'low') {
+                typeClass = "bg-success-subtle";
+                typeBorder = "border border-success";
+            } else if (currentCall.type === 'medium') {
+                typeClass = "bg-warning-subtle";
+                typeBorder = "border border-warning";
+            } else if (currentCall.type === 'high' || currentCall.type === 'critical') {
+                typeClass = "bg-danger-subtle";
+                typeBorder = "border border-danger";
+            } else {
+                console.log("Error assigning bg based on type of call")
+            }
+            ticketContainer.innerHTML +=
+                `<p class="ticket-card border-radius ${typeBorder}">
+            <strong class="type-tag border-radius ${typeClass}">${currentCall.type}</strong> 
+            <span class="caller-question">${currentCall.question}</span> <br /> <span class="caller-details">${currentCall.details}</span>
+            </p>`;
+            clearCallWindow();
+            genTicketButton.classList.add("d-none");
+            clarifyButton.classList.remove("d-none");
+        }
+    } else {
+        callButton.classList.add("d-none");
+        callAlert.innerHTML = "No one is calling...";
+        clarifyButton.classList.add("d-none");
+        alert('No calls');
+    }
+
+    // callPlaceholder.remove();
+    // offcanvasCallContainer.innerHTML += `<p>User asks: ${newCall.question}</p>`;
+    // clarifyButton.addEventListener('click', function () {
+    //     offcanvasCallContainer.innerHTML += `<p>User says: ${newCall.details}</p>`;
+    //     clarifyButton.remove();
+    //     genTicketButton.classList.remove("d-none");
+    // });
+    // genTicketButton.addEventListener('click', function () {
+    //     ticketPlaceholder.remove();
+    //     ticketContainer.innerHTML += `<strong>${newCall.type}</strong> <span class="caller-question">${newCall.question}</span> <br /> <span class="caller-details">${newCall.details}</span><br />`;
+    //     clearCallWindow();
+    // });
 });
+
+function clearCallWindow() {
+    offcanvasCallContainer.innerHTML = "";
+}
