70a5e7bc884eaedfa1651bc23b9d265e438ad752
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2024-10-20 10:23:41 -0400

    Modify HTML and js
    Modified index.html to get rid of the toasts and add an offcanvas element where the 'call' details will be.
    Modified script.js to add functionality to get a random call and put the call details in the offcanvas element.
    Also added some more interactivity so that the user can 'ask' clarifying questions by clicking a button, which will display some more details of the issue.
    Added functionality to the generate ticket button so that when a user clicks it, the ticket info will display in the ticket window.

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 console.log('yo');
-const toastTrigger = document.getElementById('liveToastBtn');
-const toastLiveExample = document.getElementById('liveToast');
+let offcanvasCallDetails = document.querySelector('.call-placeholder');
+let ticketPlaceholder = document.querySelector('.ticket-placeholder');
+let ticketContainer = document.querySelector('.ticket-container');
+let offcanvasCallContainer = document.querySelector('.call-placeholder-container');
+let callButton = document.querySelector('.call-button');
+let clarifyButton = document.querySelector('.clarify-button');
+let genTicketButton = document.querySelector('.generate-ticket-button');
 const callTypes = [
-    { type: 'low', details: 'A caller asks question 1' },
-    { type: 'medium', details: 'A caller asks question 2' },
-    { type: 'high', details: 'A caller asks question 3' },
-    { type: 'critical', details: 'A caller asks question 4' }
+    { type: 'low', question: 'A caller asks question 1', details: 'More in depth info 1' },
+    { type: 'medium', question: 'A caller asks question 2', details: 'More in depth info 2' },
+    { type: 'high', question: 'A caller asks question 3', details: 'More in depth info 3' },
+    { type: 'critical', question: 'A caller asks question 4', details: 'More in depth info 4' }
 ];
 
 function getCallFromArray(array) {
-    let type = array.type;
-    let details = array.details;
-    return type, details;
-}
-
-let newCall = getCallFromArray(callTypes);
-
-function showNewCall(call) {
-    const toast = new bootstrap.Toast(toastLiveExample);
-
-    toast.show();
-
+    let randIndex = Math.floor(Math.random() * array.length);
+    return array[randIndex];
 }
 
 
-
-function makeNewTicket(call) {
-
-}
+callButton.addEventListener('click', function () {
+    let newCall = getCallFromArray(callTypes);
+    offcanvasCallDetails.remove();
+    offcanvasCallContainer.innerHTML += `<p>User asks: ${newCall.question}</p>`;
+    clarifyButton.addEventListener('click', function () {
+        offcanvasCallContainer.innerHTML += `<p>User asks: ${newCall.details}</p>`;
+        clarifyButton.remove();
+        genTicketButton.classList.remove("d-none");
+    });
+    genTicketButton.addEventListener('click', function () {
+        ticketPlaceholder.remove();
+        ticketContainer.innerHTML += `<strong>${newCall.type}</strong> <span class="caller-question">${newCall.question}</span> <br /> <span class="caller-details">${newCall.details}</span>`;
+    })
+});
